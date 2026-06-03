5ff8b7d798b313b2f0d7cc8d0360796a93322e5c
starinoxtechnosolutions-dev <starinoxtechnosolutions@gmail.com>
2026-06-03 18:07:25 +0530

    Add files via upload

--- Argon.py
+++ Argon.py
@@ -201,5 +201,10 @@ def return_cylinder(id):
 
     return redirect("/")
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000))
+    )
